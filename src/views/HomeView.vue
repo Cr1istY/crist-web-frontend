341febc88682d17d44f57d7b9361c1d7f5db7af1
crist <cr1st4ever@outlook.com>
2025-12-09 23:18:13 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center">Welcome to Foreveryang</h1>
+          <p class="text-center">This is a simple web application that allows you to create and manage your own blog posts.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
